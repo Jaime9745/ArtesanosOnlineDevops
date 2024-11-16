1023cb0847fa9e371b9e7efc9772193d2bc10135
Jaime Rodriguez <jaimedanielmontana@gmail.com>
2024-11-16 13:57:45 -0500

    Artesanias text

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
     return (
         <div className='app-download' id='app-download'>
-            <p><br />Tomato App</p>
+            <p><br />ARTESANÍAS</p>
             <div className="app-download-platforms">
                 <img src={assets.play_store} alt="" />
                 <img src={assets.app_store} alt="" />
