cb8d39d5c76832ea1a8ddcd7c762752d3e53a7b9
Jack Blaze <ederdmc@hotmail.com>
2024-11-17 17:32:52 -0500

    Minor UI Changes

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -14,8 +14,10 @@ const ExploreMenu = ({category,setCategory}) => {
         {menu_list.map((item,index)=>{
             return (
                 <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                  <div className='explore-menu-list-item-img-container'>
                     <img src={item.menu_image} className={category===item.menu_name?"active":""} alt="" />
-                    <p>{item.menu_name}</p>
+                  </div>
+                  <p>{item.menu_name}</p> 
                 </div>
             )
         })}
